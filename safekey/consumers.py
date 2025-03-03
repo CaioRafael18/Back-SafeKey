f91957f1b38d49fdf7853b9c25caef54187be4de
CaioRafael18 <caiorafaelbelarmino1@gmail.com>
2025-03-02 22:03:24 -0300

    corrigindo consumers

--- safekey/consumers.py
+++ safekey/consumers.py
@@ -3,9 +3,10 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class RoomStatusConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        # Define o grupo WebSocket corretamente
+        self.room_group_name = "room_status_channel"
+
         # Adiciona o WebSocket ao grupo
-        self.room_name = "room_status_channel"
-        self.room_group_name = f"room_status_{self.room_name}"
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
@@ -15,16 +16,13 @@ class RoomStatusConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Desconecta o WebSocket
+        # Remove o WebSocket do grupo ao desconectar
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
         
-    # Método para enviar notificações em tempo real
+    # Método para enviar notificações WebSocket
     async def send_room_status_update(self, event):
-        # Envia uma notificação para o cliente WebSocket
         message = event['message']
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
+        await self.send(text_data=json.dumps({'message': message}))
